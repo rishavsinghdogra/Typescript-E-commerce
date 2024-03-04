93965e40c02c402ed816aa59af8b610a477c438c
Rishav Singh Dogra <rishav.vdoit@gmail.com>
2024-03-04 19:11:27 +0530

    react quill addition

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import axios from "axios";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
