7585966a18693c31f3cc6f0a105f8369fdf1a81d
Rishav Singh Dogra <rishav.vdoit@gmail.com>
2024-03-14 14:54:06 +0530

    bug fixes

--- src/components/DisplayProduct.tsx
+++ src/components/DisplayProduct.tsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import mycontext from "../contexts/Mycontex.ts";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { apiProducts } from "../services/Products.api";
 import useQuery from "../hooks/useQuery.ts";
 import CartButton from "../assets/CartButton.tsx";
@@ -25,6 +25,8 @@ const DisplayProduct = () => {
 
   let { nightTheme } = useContext(ThemeContext) as ThemeType;
 
+  const navigate = useNavigate();
+
   const { loading, setLoading } = useQuery(apiProducts, {
     onSuccess: (response: AxiosResponse) => {
       if (!isCategory) {
@@ -36,8 +38,6 @@ const DisplayProduct = () => {
       console.log(error);
     },
   });
-
-  // setIsCategory(false);
 
   const handleClick = (product: Record<string, any>) => {
     const index = clickedProducts.findIndex((p) => p.id === product.id);
@@ -88,23 +88,23 @@ const DisplayProduct = () => {
       ) : (
         // Products
         products.map((value, index) => (
-          <Link
-            to={`/productDetail/${value.id}`}
-            className="text-center text-white font-semibold mb-2 "
+          <div
+            key={index}
+            className={`product hover:scale-125 flex flex-col items-center shadow-lg rounded-md w-[150px] h-[200px] justify-center mr-4 mb-4 sm:mr-0 sm:mb-0 sm:ml-4 sm:mt-4 hover:duration-300 bg-gradient-to-br ${
+              nightTheme
+                ? "from-blue-600 to-blue-800"
+                : "from-blue-200 to-blue-400"
+            }`}
           >
-            <div
-              key={index}
-              className={`product hover:scale-125 flex flex-col items-center shadow-lg rounded-md w-[150px] h-[200px] justify-center mr-4 mb-4 sm:mr-0 sm:mb-0 sm:ml-4 sm:mt-4 hover:duration-300 bg-gradient-to-br ${
-                nightTheme
-                  ? "from-blue-600 to-blue-800"
-                  : "from-blue-200 to-blue-400"
-              }`}
+            <img
+              className="w-[100px] h-[100px] object-cover mb-2 rounded-full"
+              src={value.image}
+              alt={value.title}
+            />
+            <Link
+              to={`/productDetail/${value.id}`}
+              className="text-center text-white font-semibold mb-2 "
             >
-              <img
-                className="w-[100px] h-[100px] object-cover mb-2 rounded-full"
-                src={value.image}
-                alt={value.title}
-              />
               <div className="group text-center w-[150px] mb-4">
                 <p className="title truncate hover:text-wrap px-2">
                   {value.title.split(" ").length > 5
@@ -112,17 +112,17 @@ const DisplayProduct = () => {
                     : value.title}
                 </p>
               </div>
-              <div
-                className="cartButton inline"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleClick(value);
-                }}
-              >
-                <CartButton />
-              </div>
+            </Link>
+            <div
+              className="cartButton inline"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleClick(value);
+              }}
+            >
+              <CartButton />
             </div>
-          </Link>
+          </div>
         ))
       )}
 
